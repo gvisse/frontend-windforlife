d3e2cbb7f2c6ab7505e1c88fce34be09f76fc310
Guillaume Visse <guillaume.visse@gmail.com>
2022-11-15 10:49:14 +0100

    Add resolvers, move model and change method name

---
+++ src/app/tag/resolvers/tags.resolver.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import { Tag } from '../models/tag.model';
+import { TagsService } from '../services/tags.service';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class PostsResolver implements Resolve<Tag[]> {
+  constructor(private tagsService: TagsService) {}
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Tag[]> {
+    return this.tagsService.getTags();
+  }
+}
--- src/app/tag/services/tags.service.ts
+++ src/app/tag/services/tags.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Tag } from 'src/app/core/models/tag.model';
+import { Tag } from 'src/app/tag/models/tag.model';
 import { environment } from '../../../environments/environment';
 
 
@@ -9,7 +9,7 @@ import { environment } from '../../../environments/environment';
 export class TagsService {
   constructor(private http: HttpClient) {}
 
-  getPosts(): Observable<Tag[]> {
+  getTags(): Observable<Tag[]> {
     return this.http.get<Tag[]>(`${environment.apiUrl}/tag`);
   }
 }

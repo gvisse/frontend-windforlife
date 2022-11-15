de91e3e2b0940269cadd8f235dd2e84279028e48
Guillaume Visse <guillaume.visse@gmail.com>
2022-11-15 13:32:44 +0100

    Add route 'tag' to app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'anemometer', loadChildren: () => import('./anemometer/anemometer.module').then(m => m.AnemometerModule) },
   { path: 'tag', loadChildren: () => import('./tag/tag.module').then(m => m.TagModule) },
+  { path: 'anemometer', loadChildren: () => import('./anemometer/anemometer.module').then(m => m.AnemometerModule) },
+  { path: 'wind', loadChildren: () => import('./wind/wind.module').then(m => m.WindModule) },
   { path: '**', redirectTo: 'tag'}
 ];
 

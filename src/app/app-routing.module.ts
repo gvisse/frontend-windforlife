9e58a6918026a5638680af0e3bcd030ea05c4bd5
Guillaume Visse <guillaume.visse@gmail.com>
2022-11-15 09:45:09 +0100

    Add header material, route and style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'anemometer', loadChildren: () => import('./anemometer/anemometer.module').then(m => m.AnemometerModule) },
+  { path: '**', redirectTo: 'anemometer'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

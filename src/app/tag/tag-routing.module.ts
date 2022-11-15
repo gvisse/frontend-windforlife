2279e2c3b2cb9696c124def225c5ee3fb0e177ce
Guillaume Visse <guillaume.visse@gmail.com>
2022-11-15 11:04:23 +0100

    Add route for TagListComponent

--- src/app/tag/tag-routing.module.ts
+++ src/app/tag/tag-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TagListComponent } from './components/tag-list/tag-list.component';
+import { TagsResolver } from './resolvers/tags.resolver';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', component: TagListComponent, resolve: { posts: TagsResolver } }
+];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]

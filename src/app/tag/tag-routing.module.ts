1dc55a988e11c79288b045445089cad0ea502eed
Guillaume Visse <guillaume.visse@gmail.com>
2022-11-22 13:17:40 +0100

    Delete route tag/new

--- src/app/tag/tag-routing.module.ts
+++ src/app/tag/tag-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NewTagComponent } from './components/new-tag/new-tag.component';
 import { TagListComponent } from './components/tag-list/tag-list.component';
 
 const routes: Routes = [
-  { path: 'new', component: NewTagComponent},
   { path: '', component: TagListComponent }
 ];
 @NgModule({

2f1b0d9f25d99cea9a71fae4c3a403ce91f3f77a
Guillaume Visse <guillaume.visse@gmail.com>
2022-11-23 16:16:58 +0100

    Add route and change buttons/links style

--- src/app/anemometer/anemometer-routing.module.ts
+++ src/app/anemometer/anemometer-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { AnemometerListComponent } from './components/anemometer-list/anemometer-list.component';
 import { SingleAnemometerComponent } from './components/single-anemometer/single-anemometer.component';
 import { AnemometerNewComponent } from './components/anemometer-new/anemometer-new.component';
+import { AnemometerUpdateComponent } from './components/anemometer-update/anemometer-update.component';
 
 const routes: Routes = [
   { path: 'new', component: AnemometerNewComponent},
+  { path: ':id/update', component: AnemometerUpdateComponent },
   { path: ':id', component: SingleAnemometerComponent },
   { path: '', component: AnemometerListComponent }
 ];

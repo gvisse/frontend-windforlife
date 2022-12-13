f5e7cfeeba047e861804db4bea9e850708e3eabc
Guillaume Visse <guillaume.visse@gmail.com>
2022-12-13 14:20:31 +0100

    Configure JwtModule in appModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { JwtModule } from '@auth0/angular-jwt';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
 import { AuthModule } from './auth/auth.module';
+
+export function tokenGetter(): string {
+  return localStorage.getItem('token') || '';
+}
 
 @NgModule({
   declarations: [
@@ -16,7 +22,15 @@ import { AuthModule } from './auth/auth.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     CoreModule,
-    AuthModule
+    AuthModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter,
+        allowedDomains: ['localhost:4200', '127.0.0.1:4200'],
+        disallowedRoutes: [],
+        skipWhenExpired: true,
+      }
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]

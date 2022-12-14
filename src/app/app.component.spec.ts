bb7a7b594fa48811ea90bb7082e74376b08b69f4
Guillaume Visse <guillaume.visse@gmail.com>
2022-12-14 16:46:48 +0100

    Create test for AppComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,60 @@
-import { TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
+@Component({selector: 'app-header', template: ''})
+class HeaderStubComponent{}
+
+@Component({selector: 'router-outlet', template: ''})
+class RouterOutletStubComponent{}
+
+@Component({selector: 'app-footer', template: ''})
+class FooterStubComponent{}
+
+
 describe('AppComponent', () => {
+
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
       ],
       declarations: [
-        AppComponent
+        AppComponent, RouterOutletStubComponent, HeaderStubComponent, FooterStubComponent
       ],
     }).compileComponents();
   });
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
   it(`should have as title 'frontend-windforlife'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('frontend-windforlife');
+    expect(component.title).toEqual('frontend-windforlife');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
+  it('should render header', () => {
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('frontend-windforlife app is running!');
+    expect(compiled.querySelector('app-header')?.textContent).toBe('')
+  });
+
+  it('should render router-outlet', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.main-content router-outlet')?.textContent).toBe('')
+  });
+
+  it('should render footer', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('app-footer')?.textContent).toBe('');
   });
 });

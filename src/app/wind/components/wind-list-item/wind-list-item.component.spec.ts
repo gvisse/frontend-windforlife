8b0211413731cae48a148dc87f330fd740b8aad5
Guillaume Visse <guillaume.visse@gmail.com>
2022-12-14 17:34:28 +0100

    Adding render french time test on wind list item

--- src/app/wind/components/wind-list-item/wind-list-item.component.spec.ts
+++ src/app/wind/components/wind-list-item/wind-list-item.component.spec.ts
@@ -1,23 +1,63 @@
+import { formatDate, registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+import { DebugElement, LOCALE_ID } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { Wind } from '../../models/wind.model';
 
 import { WindListItemComponent } from './wind-list-item.component';
+
+registerLocaleData(localeFr);
 
 describe('WindListItemComponent', () => {
   let component: WindListItemComponent;
   let fixture: ComponentFixture<WindListItemComponent>;
 
+  let windDe: DebugElement;
+  let windEl: HTMLElement;
+
+  let expectedWind: Wind;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ WindListItemComponent ]
+      declarations: [ WindListItemComponent ],
+      providers: [{
+        provider: LOCALE_ID, useValue: 'fr-FR'
+      }]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(WindListItemComponent);
     component = fixture.componentInstance;
+
+    windDe = fixture.debugElement.query(By.css('.wind'))
+    windEl = windDe.nativeElement;
+
+    expectedWind = {id: 42, speed: 4.2, time: new Date(), direction: 90, cardinal: 'E', anemometer_id: 5}
+
+    component.wind = expectedWind;
+    component.canDelete = true;
+
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create WindListItemComponent', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display wind speed', () => {
+    const expectedSpeed = expectedWind.speed;
+    expect(windEl.textContent).toContain(`${expectedSpeed}`);
+  });
+
+  it('should display wind time', () => {
+    const expectedTime = formatDate(expectedWind.time, 'dd/MM/YY, à HH:mm', 'fr-FR');
+    expect(windEl.textContent).toContain(expectedTime);
+  });
+
+  it('should display wind direction and cardinal', () => {
+    const expectedDirection = expectedWind.direction;
+    const expectedCardinal = expectedWind.cardinal;
+    expect(windEl.textContent).toContain(`${expectedDirection}° / ${expectedCardinal}`);
+  })
 });

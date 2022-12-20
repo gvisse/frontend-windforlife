795b062ce6cf980a5eeee1e9b5a6433f46d40995
Guillaume Visse <guillaume.visse@gmail.com>
2022-12-20 15:18:39 +0100

    Create test for anemometer list item

--- src/app/anemometer/components/anemometer-list-item/anemometer-list-item.component.spec.ts
+++ src/app/anemometer/components/anemometer-list-item/anemometer-list-item.component.spec.ts
@@ -1,23 +1,39 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { AnemometerListItemComponent } from './anemometer-list-item.component';
+import { Anemometer } from '../../models/anemometer.model';
+import { Router } from '@angular/router';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('AnemometerListItemComponent', () => {
   let component: AnemometerListItemComponent;
   let fixture: ComponentFixture<AnemometerListItemComponent>;
+  let router: Router;
+
+  let anemometer: Anemometer;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AnemometerListItemComponent ]
-    })
-    .compileComponents();
+      declarations: [ AnemometerListItemComponent ],
+      imports: [ RouterTestingModule ],
+      schemas: [NO_ERRORS_SCHEMA]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(AnemometerListItemComponent);
     component = fixture.componentInstance;
+    anemometer = {id: 1, name: 'test Anemometer', latitude: '1.25', longitude: '0.245', altitude: 0};
+    component.anemometer = anemometer;
+    router = TestBed.inject(Router);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should navigate to the anemometer detail page on view anemometer', () => {
+    const navigateSpy = jest.spyOn(router, 'navigateByUrl');
+    component.onViewAnemometer();
+    expect(navigateSpy).toHaveBeenCalledWith('anemometer/1');
   });
 });
--- src/app/anemometer/components/anemometer-list-item/anemometer-list-item.component.ts
+++ src/app/anemometer/components/anemometer-list-item/anemometer-list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, NgZone, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Anemometer } from '../../models/anemometer.model';
 
@@ -11,13 +11,13 @@ export class AnemometerListItemComponent implements OnInit {
 
   @Input() anemometer!: Anemometer;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private ngZone: NgZone) { }
 
   ngOnInit(): void {
   }
 
   onViewAnemometer(){
-    this.router.navigateByUrl(`anemometer/${this.anemometer.id}`);
+    this.ngZone.run(() => this.router.navigateByUrl(`anemometer/${this.anemometer.id}`));
   }
 
 }

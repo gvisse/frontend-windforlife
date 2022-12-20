3a9129e1e006083687df0d03cf6abda2539279c1
Guillaume Visse <guillaume.visse@gmail.com>
2022-12-20 11:56:08 +0100

    Change create test for HeaderComponent

--- src/app/core/components/header/header.component.spec.ts
+++ src/app/core/components/header/header.component.spec.ts
@@ -1,4 +1,8 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { JwtHelperService, JWT_OPTIONS } from '@auth0/angular-jwt';
+import { MaterialModule } from '../../../shared/material.module';
+import { AuthService } from '../../services/auth.service';
 
 import { HeaderComponent } from './header.component';
 
@@ -6,14 +10,26 @@ describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
 
+  let authService: AuthService;
+  let httpMock: HttpTestingController;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      imports: [
+        HttpClientTestingModule,
+        MaterialModule
+      ],
+      declarations: [ HeaderComponent ],
+      providers: [AuthService, JwtHelperService,
+        { provide: JWT_OPTIONS, useValue: JWT_OPTIONS }]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
+
+    authService = TestBed.inject(AuthService);
+    httpMock = TestBed.inject(HttpTestingController);
     fixture.detectChanges();
   });
 

6756c8cc6fba9c17c9132e3c983b8d1f6c5d7499
Guillaume Visse <guillaume.visse@gmail.com>
2022-12-20 14:32:08 +0100

    Configure coverage run on jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,14 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig');
+
 module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   globalSetup: 'jest-preset-angular/global-setup',
+  collectCoverage: true,
+  coverageReporters: ['html'],
+  coverageDirectory: 'coverage/my-app',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, {
+    prefix: '<rootDir>/'
+  })
 };

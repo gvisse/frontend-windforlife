54029f106a5b356b31bb17ee6cc572d63398c71a
Guillaume Visse <guillaume.visse@gmail.com>
2022-12-28 13:47:06 +0100

    Add createWind, goToPage and deleteWInd tests

--- src/app/wind/services/winds.service.spec.ts
+++ src/app/wind/services/winds.service.spec.ts
@@ -9,43 +9,43 @@ describe('WindsService', () => {
 	let service: WindsService;
 	let httpMock: HttpTestingController;
 
-    const date2022 = new Date(2022, 12, 31);
-    const date2023 = new Date(2023, 1, 1);
+	const date2022 = new Date(2022, 12, 31, 9, 35, 10);
+	const date2023 = new Date(2023, 1, 1, 10, 15, 25);
 
-    const mockWind1 = {
-        id: 1,
-        speed: 5,
-        direction: 90,
-        cardinal: 'E',
-        time: date2022,
-        anemometer_id: 1
-    }
+	const mockWind1 = {
+		id: 1,
+		speed: 5,
+		direction: 90,
+		cardinal: 'E',
+		time: date2022,
+		anemometer_id: 1
+	}
 
-    const mockWind2 = {
-        id: 2,
-        speed: 2.5,
-        direction: 0,
-        cardinal: 'N',
-        time: date2023,
-        anemometer_id: 1
-    }
+	const mockWind2 = {
+		id: 2,
+		speed: 2.5,
+		direction: 0,
+		cardinal: 'N',
+		time: date2023,
+		anemometer_id: 1
+	}
 
-    function flushRequest(url: string, method: string, response: {}) {
+	function flushRequest(url: string, method: string, response: {}) {
 		const req = httpMock.expectOne(url);
 		expect(req.request.method).toBe(method);
 		req.flush(response);
 	}
 
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-            imports: [HttpClientTestingModule],
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			imports: [HttpClientTestingModule],
 			providers: [WindsService]
 		});
 		service = TestBed.inject(WindsService);
 		httpMock = TestBed.inject(HttpTestingController);
-    });
+	});
 
-    afterEach(() => {
+	afterEach(() => {
 		httpMock.verify();
 	});
 
@@ -53,7 +53,7 @@ describe('WindsService', () => {
 		expect(service).toBeTruthy();
 	});
 
-    it('should fetch winds from the server', () => {
+	it('should fetch winds from the server', () => {
 		const winds = [mockWind1, mockWind2];
 		const mockResponse = {
 			results: winds,
@@ -66,7 +66,7 @@ describe('WindsService', () => {
 		flushRequest(`${environment.apiUrl}/wind/`, 'GET', mockResponse);
 	});
 
-    it('should not call the server if winds have been loaded in the last 5 minutes', () => {
+	it('should not call the server if winds have been loaded in the last 5 minutes', () => {
 		service.getWindsFromServeur();
 		service.winds$.subscribe(winds => {
 			expect(winds).toEqual([mockWind1, mockWind2]);
@@ -83,5 +83,56 @@ describe('WindsService', () => {
 		});
 		flushRequest(`${environment.apiUrl}/wind/?&anemometer_id=1`, 'GET', [mockWind1, mockWind2]);
 	});
-	
+
+
+	it('should create a wind', () => {
+		const formValue = { speed: 5, direction: 90, time: date2022, anemometer_id: 1 }
+		service.createWind(formValue);
+		flushRequest(`${environment.apiUrl}/wind/`, 'POST', {});
+		flushRequest(`${environment.apiUrl}/wind/`, 'GET', [mockWind1]);
+	});
+
+	it('should delete a wind', () => {
+		const formValue = { speed: 5, direction: 90, time: date2022, anemometer_id: 1 }
+		service.createWind(formValue);
+		flushRequest(`${environment.apiUrl}/wind/`, 'POST', {});
+		flushRequest(`${environment.apiUrl}/wind/`, 'GET', [mockWind1]);
+		service.deleteWind(1);
+		flushRequest(`${environment.apiUrl}/wind/1`, 'DELETE', {});
+		flushRequest(`${environment.apiUrl}/wind/`, 'GET', []);
+	});
+
+	it('should go to page X with N wind of page size', () => {
+		jest.spyOn(service, 'winds$', 'get').mockReturnValue(of([
+			{ id: 11, speed: 1, direction: 5, cardinal: 'N', time: date2022, anemometer_id:1 },
+			{ id: 12, speed: 5, direction: 0, cardinal: 'N', time: date2023, anemometer_id:1 }
+		]));
+		jest.spyOn(service, 'countWinds$', 'get').mockReturnValue(of(12));
+		jest.spyOn(service, 'loading$', 'get').mockReturnValue(of(false));
+		const mockResponse = {
+			count: 12,
+			next: null,
+			previous: `${environment.apiUrl}/wind/`,
+			results: [
+				{ id: 11, speed: 1, direction: 5, cardinal: 'N', time: date2022, anemometer_id:1 },
+				{ id: 12, speed: 5, direction: 0, cardinal: 'N', time: date2023, anemometer_id:1 }
+			]
+		};
+
+		service.goToPage(undefined, { page: 2, size: 10 });
+		flushRequest(`${environment.apiUrl}/wind/?&page=2&page_size=10`, 'GET', mockResponse);
+
+		service.winds$.subscribe(winds => {
+			expect(winds).toEqual([
+				{ id: 11, speed: 1, direction: 5, cardinal: 'N', time: date2022, anemometer_id:1 },
+			{ id: 12, speed: 5, direction: 0, cardinal: 'N', time: date2023, anemometer_id:1 }
+			]);
+		});
+		service.countWinds$.subscribe(count => {
+			expect(count).toEqual(12);
+		});
+		service.loading$.subscribe(loading => {
+			expect(loading).toBeFalsy();
+		});
+	});
 });
